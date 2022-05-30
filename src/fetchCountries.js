66ad0ad37c3ae8b08f6730e18ba4b3c70ddd4eda
Natalia29011989 <tjomamon@gmail.com>
2022-05-31 00:26:58 +0300

    выполнено по дебаунс

---
+++ src/fetchCountries.js
@@ -0,0 +1,6 @@
+export const fetchCountries = (name) => {
+    return fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages`).then(response => {
+        return response.json();
+    })
+}     
+
--- src/index.js
+++ src/index.js
@@ -1,3 +1,58 @@
 import './css/styles.css';
+import { fetchCountries } from "../src/fetchCountries";
+// import  _.debounce from "lodash.debounce";
+var debounce = require('lodash.debounce');
+
+const input = document.querySelector('input#search-box')
 
 const DEBOUNCE_DELAY = 300;
+let cauntry = 'peru';
+
+
+
+
+input.addEventListener('input', debounce((e) => {
+    cauntry = input.value;
+    console.log(cauntry);
+    fetchCountries(cauntry).then(data => {
+  console.log(data);
+  })
+  .catch(error => {
+   console.log(error);
+  });
+},DEBOUNCE_DELAY))
+
+
+ 
+
+
+//   .then(data => {
+//   console.log(data);
+//   })
+//   .catch(error => {
+//    console.log(error);
+//   });
+
+
+
+
+// fetch('https://restcountries.com/v2/name/pery').then(response => {
+//     return response.json();
+   
+// }).then(cauntry => {
+//         console.log(cauntry);
+//     })
+
+// fetch('https://restcountries.com/v2/name/peru')
+//   .then(response => {
+//     if (!response.ok) {
+//       throw new Error(response.status);
+//     }
+//     return response.json();
+//   })
+//   .then(data => {
+//   console.log(data);
+//   })
+//   .catch(error => {
+//    console.log(error);
+//   });
