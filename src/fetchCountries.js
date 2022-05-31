166da04470e21119ee94682bfeb76ad6a44d0fe5
Natalia29011989 <tjomamon@gmail.com>
2022-05-31 17:05:03 +0300

    черновик

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -1,5 +1,8 @@
 export const fetchCountries = (name) => {
     return fetch(`https://restcountries.com/v2/name/${name}?fields=name,capital,population,flag,languages`).then(response => {
+         if (!response.ok) {
+      throw new Error(response.status);
+    }
         return response.json();
     })
 }     
--- src/index.js
+++ src/index.js
@@ -1,58 +1,86 @@
 import './css/styles.css';
 import { fetchCountries } from "../src/fetchCountries";
-// import  _.debounce from "lodash.debounce";
+import Notiflix from 'notiflix';
 var debounce = require('lodash.debounce');
 
 const input = document.querySelector('input#search-box')
+const countryUl = document.querySelector('ul.country-list')
+const countryInfo = document.querySelector('.country-info')
 
 const DEBOUNCE_DELAY = 300;
-let cauntry = 'peru';
+let country = '';
 
 
 
 
 input.addEventListener('input', debounce((e) => {
-    cauntry = input.value;
-    console.log(cauntry);
-    fetchCountries(cauntry).then(data => {
-  console.log(data);
+  country = input.value.trim();
+    countryInfo.innerHTML = '';
+      countryUl.innerHTML = '';
+  if (country === '') {
+    
+    return
+  
+
+  } 
+    console.log(country);
+  fetchCountries(country).then(data => {
+  
+    console.log(data);
+
+    if (data.length > 10)
+    Notiflix.Notify.info("Too many matches found. Please enter a more specific name.");
+    if (data.length >= 2 && data.length <= 10) {
+      const markup = data.map(({ flag, name }) => 
+         `<li>
+        <img src=${flag}
+        alt="flag"
+        width="27"
+        height="auto">
+        <div><p>${name}</p></div></li>`
+      ).join(""); 
+      countryUl.insertAdjacentHTML('beforeend', markup);
+      
+    }
+    if (data.length === 1) {
+    
+        countryUl.innerHTML = '';
+        const oneCountry = data.map(({ name, capital, population, flag, languages }) => 
+        `
+        <div class="div-flex">
+        <img src=${flag}
+        alt="flag"
+        width="27"
+        height="auto">
+      
+        <div class="name"><h2>${name}</h2></div>
+        </div>
+        <ul class="countriData">
+            <li class="item-countriData">
+                <span class="fatty-span">Capital:</span><span class="span">${capital}</span>
+            </li>
+            <li  class="item-countriData">
+                <span class="fatty-span">Population:</span><span class="span">${population}</span>
+            </li>
+            <li  class="item-countriData">
+                <span class="fatty-span">Languages:</span><span class="span">${languages.map(i => ` ${i.name}`)}</span>
+            </li>
+        </ul>
+        `
+      ).join(""); 
+      countryInfo.insertAdjacentHTML('beforeend', oneCountry);
+    }
+       
   })
   .catch(error => {
-   console.log(error);
+  Notiflix.Notify.failure("Oops, there is no country with that name");
   });
 },DEBOUNCE_DELAY))
 
-
- 
-
-
-//   .then(data => {
-//   console.log(data);
-//   })
-//   .catch(error => {
-//    console.log(error);
-//   });
-
-
-
-
-// fetch('https://restcountries.com/v2/name/pery').then(response => {
-//     return response.json();
-   
-// }).then(cauntry => {
-//         console.log(cauntry);
-//     })
-
-// fetch('https://restcountries.com/v2/name/peru')
-//   .then(response => {
-//     if (!response.ok) {
-//       throw new Error(response.status);
-//     }
-//     return response.json();
-//   })
-//   .then(data => {
-//   console.log(data);
-//   })
-//   .catch(error => {
-//    console.log(error);
-//   });
+// const ulGallery = document.querySelector("ul.gallery");
+// const gallery = images.map(({ url, alt }) => 
+//   `<li class="item-gallery">
+//   <img class="image-gallery" src=${url} alt ="${alt}" width="300" height="180" />
+// </li>`
+// ).join("");
+// ulGallery.insertAdjacentHTML("afterbegin", gallery);
